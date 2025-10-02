c9f44e00ab7561e72eb84aa60f3ea256f362f3d6
Aziz Khan <azizkhanrules7@gmail.com>
2025-10-02 16:29:22 +1000

    Update countdown-timer.py

--- Projects/countdown-timer.py
+++ Projects/countdown-timer.py
@@ -2,15 +2,23 @@
 
 import time
 
+# Collect user name
+
 print("Welcome to the Simple countdown timer program in Python")
-print("-----")
 name = input("Please enter your full name: ")
-print("-----")
 print(f"Hello {name}")
 
-print("-----")
+# Enter time in seconds
+
 my_time = int(input("Enter the time in seconds: "))
-print("-----")
+
+# While loop for an entering an invalid time
+
+while my_time <= 0:
+    print(f"{my_time} is not a valid time")
+    my_time = int(input("Enter the time in seconds: "))
+
+# Timer program (for loop)
 
 for x in range(my_time, 0, -1):
     seconds = x % 60
@@ -18,5 +26,5 @@ for x in range(my_time, 0, -1):
     hours = int(x / 3600)
     print(f"{hours:02}:{minutes:02}:{seconds:02}")
     time.sleep(1)
-print("-----")
+
 print("TIME'S UP!")
